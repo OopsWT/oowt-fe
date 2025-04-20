64728aba24f4e05b5f853f5cdd9f8e1124c28384
piotr tylicki <piotr.tylicki@gmail.com>
2025-04-20 12:19:41 +0200

    add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,6 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const globalData = await getGlobalData();
-  console.dir(globalData, { depth: null });
   return (
     <html lang="en">
       <body
