3322c530ac9490a79fc181870a49df6f65e01a29
piotr tylicki <piotr.tylicki@gmail.com>
2025-06-09 00:48:14 +0200

    fix images url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,5 +22,5 @@ export function getStrapiMedia(url: string | null) {
   if (url == null) return null;
   if (url.startsWith("data:")) return url;
   if (url.startsWith("http") || url.startsWith("//")) return url;
-  return `${getStrapiURL()}${url}`;
+  return `${getStrapiURL()}${url}`.replace(/([^:]\/)\/+/g, "$1");
 }
