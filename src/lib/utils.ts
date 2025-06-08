50d5f0e43f77464156a8c1c03a347ce923c537a5
piotr tylicki <piotr.tylicki@gmail.com>
2025-06-09 00:56:16 +0200

    fix images url with two slashes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,5 +22,7 @@ export function getStrapiMedia(url: string | null) {
   if (url == null) return null;
   if (url.startsWith("data:")) return url;
   if (url.startsWith("http") || url.startsWith("//")) return url;
-  return `${getStrapiURL()}${url}`.replace(/([^:]\/)\/+/g, "$1");
+  return `${getStrapiURL()}${url}`
+    .replace(/([^:]\/)\/+/g, "$1")
+    .replace(/(%2F)+/g, "%2F");
 }
